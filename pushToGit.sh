13394ddc4cf2583600ff1828719173fc6f5d0b05
Hippo <support@mrsoir.com>
2019-09-18 20:25:28 +0200

    bashed commit

--- pushToGit.sh
+++ pushToGit.sh
@@ -7,6 +7,6 @@ git add ./public
 git add ./package.json
 git add ./pushToGit.sh
 git commit -m "bashed commit"
-#git remote rm origin
+git remote rm origin
 git remote add origin https://github.com/MrSoir/DataVisualizationThree.git
 git push -u origin master
